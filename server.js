a80c4cf955de827951f66c6063e1dc45ae58fa13
paulkitchener <paulkitchener@hotmail.com>
2019-04-21 13:19:47 +0100

    testing git from the command line3

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res){
-  res.send('Hello world from Distelli & Docker!');
+  res.send(' Hello world from Distelli & Docker!');
 });
 
 app.listen(PORT);
-console.log('Running on http://localhost: ' + PORT);
+console.log(' Running on http://localhost: ' + PORT);
